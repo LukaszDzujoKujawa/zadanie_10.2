243d29669271161c0a27a165ff8dd6ce1ee21c3e
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-05-23 22:15:00 +0200

    JS change

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,10 +1,10 @@
 $(function(){
 	console.log('DOM loaded');
 	var span = $("span");
-		span.each(function(index, element) {
-			if(index % 2 === 0){
-			$(element).css('color', 'red');
-			}
+	span.each(function(index, element) {
+	if(index % 2 === 0){
+	$(element).css('color', 'red');
+	}
 });
 
 	var paragraphs = $('p');
